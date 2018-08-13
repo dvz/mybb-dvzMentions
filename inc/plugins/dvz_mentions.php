5d4fa3d6239a7414e164c57e0b1ec7a8f652acc5
Tomasz Mlynski <devilshakerz@gmail.com>
2018-08-13 15:10:33 +0200

    1.0.5 meta

--- inc/plugins/dvz_mentions.php
+++ inc/plugins/dvz_mentions.php
@@ -47,7 +47,7 @@ function dvz_mentions_info()
         'website'       => 'https://devilshakerz.com/',
         'author'        => 'Tomasz \'Devilshakerz\' Mlynski',
         'authorsite'    => 'https://devilshakerz.com/',
-        'version'       => '1.0.4',
+        'version'       => '1.0.5',
         'codename'      => 'dvz_mentions',
         'compatibility' => '18*',
     ];
